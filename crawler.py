8429cc451966c186292d2059ee33bc4fa1f5b983
SSUN <rlatjsghks4647@naver.com>
2025-05-02 11:51:26 +0900

    [FEAT] save_book book list 넘겨주도록 변경 #1

--- crawler.py
+++ crawler.py
@@ -5,18 +5,20 @@ from BookList import fetch_books_with_filter
 from SaveBook import save_book
 
 def crawler():
-    print("크롤러 시작")
+    try:
+        print("크롤러 시작")
 
-    driver = get_driver()
-    print("드라이버 생성 완료")
+        driver = get_driver()
+        print("드라이버 생성 완료")
 
-    books = fetch_books_with_filter(driver)
-    print(f"책 {len(books)}권 수집됨")
+        books = fetch_books_with_filter(driver)
+        print(f"책 {len(books)}권 수집됨")
 
-    for book in books:
-        print(book)
-        save_book(book)
-    driver.quit()
+        save_book(books)
+        driver.quit()
+        print("크롤링 종료")
+    except Exception as e:
+        print(f"크롤러 에러 발생: {e}", flush=True)
 
 # 매주 목요일 오후 3시에 실행
 schedule.every().thursday.at("15:00").do(crawler)
